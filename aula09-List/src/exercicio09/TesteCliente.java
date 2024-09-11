51b0476d0beceb7d41b5d72c20ca74ad93bd6471
GabyReissinger <psigabriela.salgado@gmail.com>
2024-09-11 19:53:34 -0300

    Feat Aula 11

--- aula09-List/src/exercicio09/TesteCliente.java
+++ aula09-List/src/exercicio09/TesteCliente.java
@@ -8,15 +8,33 @@ public class TesteCliente {
 	public static void main(String[] args) {
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 		Scanner sc = new Scanner(System.in);
-		
+
 		while (true) {
 			System.out.println("ID do cliente: ");
 			int id = sc.nextInt();
-			
-			if (id < 0);
-				break;
-		}
-		
-	}
 
+			if (id < 0) {
+				;
+				break;
+			}
+			sc.nextLine();
+			System.out.println("Cliente: ");
+			String nome = sc.nextLine();
+
+			System.out.println("Idade: ");
+			int idade = sc.nextInt();
+
+			sc.nextLine();
+			System.out.println("Telefone: ");
+			int telefone = sc.nextInt();
+
+			Cliente cliente = new Cliente(id, nome, idade, telefone);
+			clientes.add(cliente);
+		}
+		System.out.println("\n----- Lista de Clientes -----");
+		for (Cliente cliente : clientes) {
+			System.out.println(cliente);
+		}
+		sc.close();
+	}
 }
